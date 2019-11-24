7140459c51e9c87e79e01cfafdf17a95ab913e25
Mark van der Laan <markkvdlaan93@gmail.com>
2019-11-24 21:14:34 +0100

    Make script compatible with terraform version 0.11

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,19 @@
-variable "AWS_USER_ACCESS_KEY" {}
-variable "AWS_USER_SECRET_KEY" {}
-variable "AWS_REGION" {}
-variable "AWS_AMI_UBUNTU_SERVER_16_04_LTS" {
+variable "aws_ec2_access_key" {}
+variable "aws_ec2_secret_key" {}
+variable "aws_region" {}
+variable "aws_ami_ubuntu_server_16_04_lts" {
     default = "ami-0987ee37af7792903"
 }
 
 provider "aws" {
-    access_key  = "${var.AWS_USER_ACCESS_KEY}"
-    secret_key  = "${var.AWS_USER_SECRET_KEY}"
-    region      = "${var.AWS_REGION}"
+    access_key  = "${var.aws_ec2_access_key}"
+    secret_key  = "${var.aws_ec2_secret_key}"
+    region      = "${var.aws_region}"
+    version     = "~> 2.0"
 }
 
 resource "aws_instance" "example" {
-    ami = "${var.AWS_AMI_UBUNTU_SERVER_16_04_LTS}"
+    ami = "${var.aws_ami_ubuntu_server_16_04_lts}"
     instance_type = "t2.micro"
     tags {
         Name = "example"
