3f50364fae36140603878ff5c069b23cf389cdb0
Mark van der Laan <markkvdlaan93@gmail.com>
2019-11-26 21:31:45 +0100

    Use dedicated profile

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    profile     = "${var.aws_profile}"
     access_key  = "${var.aws_ec2_access_key}"
     secret_key  = "${var.aws_ec2_secret_key}"
     region      = "${var.aws_region}"
+    profile     = "${var.aws_profile}"
     version     = "~> 2.0"
 }
 
