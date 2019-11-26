32345283c194b434506207b06f1abb78ba347ad6
Mark van der Laan <markkvdlaan93@gmail.com>
2019-11-26 21:01:12 +0100

    Use S3 as terraform backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,10 +12,19 @@ provider "aws" {
     version     = "~> 2.0"
 }
 
-resource "aws_instance" "example" {
-    ami = "${var.aws_ami_ubuntu_server_16_04_lts}"
-    instance_type = "t2.micro"
-    tags {
-        Name = "example"
-    }
+# Backend configurations can't be interpolated by variables. Therefore, use hardcoded stringss
+terraform {
+  backend "s3" {
+    bucket          = "mvdlaan93-tfstate"
+    key             = "states/terraform.tfstate"
+    region          = "eu-west-1"
+  }
 }
+
+# resource "aws_instance" "example" {
+#     ami = "${var.aws_ami_ubuntu_server_16_04_lts}"
+#     instance_type = "t2.micro"
+#     tags {
+#         Name = "example"
+#     }
+# }
